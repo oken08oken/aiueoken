92d1ba68e91859a472e637ceef07e05e12cc6715
oken08oken <oken08web@gmail.com>
2025-09-25 04:41:43 +0900

    fix

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@ const siteWidth = document.querySelector(".site-width");
 window.addEventListener("resize", (e) => {
   siteWidth.textContent = window.innerWidth;
 });
+
+const raContentOver = document.querySelectorAll(".ra-over");
+const raContentUnder = document.querySelectorAll(".ra-under");
